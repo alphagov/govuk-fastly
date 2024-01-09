78b572e75e0a78b89ea5dd48e04f213838d96987
Chris Banks <chris@banks.cx>
2024-01-09 15:20:04 +0000

    Style cleanup.
    Use `try()` everywhere rather than a mixture of `try()` and `lookup()`.
    `lookup()` is essentially redundant these days.
    
    Remove an unnecessary `${}` expression.

--- modules/www/service.tf
+++ modules/www/service.tf
@@ -39,7 +39,7 @@ locals {
         "${path.module}/_multivariate_tests.vcl.tftpl",
         { ab_tests = try(var.configuration["ab_tests"], []) }
       )
-      module_path = "${path.module}"
+      module_path = path.module
     },
     var.configuration,
     var.secrets
@@ -62,7 +62,7 @@ resource "fastly_service_vcl" "service" {
 
   dynamic "condition" {
     for_each = {
-      for c in lookup(local.template_values, "conditions", []) : c.name => c
+      for c in try(local.template_values["conditions"], []) : c.name => c
     }
     iterator = each
     content {
@@ -75,18 +75,18 @@ resource "fastly_service_vcl" "service" {
 
   dynamic "backend" {
     for_each = {
-      for b in lookup(local.template_values, "backends", []) : b.name => b
+      for b in try(local.template_values["backends"], []) : b.name => b
     }
     iterator = each
     content {
       name              = each.key
       address           = each.value.address
       use_ssl           = true
-      request_condition = lookup(each.value, "request_condition", "")
-      port              = lookup(each.value, "port", 443)
+      request_condition = try(each.value["request_condition"], "")
+      port              = try(each.value["port"], 443)
       ssl_cert_hostname = each.value.address
       ssl_sni_hostname  = each.value.address
-      shield            = lookup(each.value, "shield", "london-uk")
+      shield            = try(each.value["shield"], "london-uk")
       keepalive_time    = 0
       healthcheck       = ""
       max_tls_version   = ""
@@ -108,13 +108,13 @@ resource "fastly_service_vcl" "service" {
 
   dynamic "logging_splunk" {
     for_each = {
-      for splunk in lookup(var.secrets, "splunk", []) : splunk.name => splunk
+      for splunk in try(var.secrets["splunk"], []) : splunk.name => splunk
     }
     iterator = each
     content {
       name           = "Splunk"
       format_version = 2
-      format = lookup(each.value, "format", chomp(
+      format = try(each.value["format"], chomp(
         <<-EOT
         {
           "time": %%{time.start.sec}V,
@@ -130,13 +130,13 @@ resource "fastly_service_vcl" "service" {
       token        = each.value.token
       url          = each.value.url
       use_tls      = true
-      response_condition = lookup(each.value, "response_condition", null)
+      response_condition = try(each.value["response_condition"], null)
     }
   }
 
   dynamic "logging_s3" {
     for_each = {
-      for s3 in lookup(var.secrets, "s3", []) : s3.name => s3
+      for s3 in try(var.secrets["s3"], []) : s3.name => s3
     }
     iterator = each
     content {
@@ -148,14 +148,14 @@ resource "fastly_service_vcl" "service" {
       redundancy = each.value.redundancy
       s3_access_key = each.value.access_key_id
       s3_secret_key = each.value.secret_access_key
-      response_condition = lookup(each.value, "response_condition", null)
+      response_condition = try(each.value["response_condition"], null)
 
       format_version = 2
       message_type = "blank"
       gzip_level = 9
       timestamp_format = "%Y-%m-%dT%H:%M:%S.000"
 
-      format = lookup(each.value, "format", chomp(
+      format = try(each.value["format"], chomp(
         <<-EOT
         { "client_ip":"%%{json.escape(client.ip)}V", "request_received":"%%{begin:%Y-%m-%d %H:%M:%S.}t%%{time.start.msec_frac}V", "request_received_offset":"%%{begin:%z}t", "method":"%%{json.escape(req.method)}V", "url":"%%{json.escape(req.url)}V", "status":%>s, "request_time":%%{time.elapsed.sec}V.%%{time.elapsed.msec_frac}V, "time_to_generate_response":%%{time.to_first_byte}V, "bytes":%B, "content_type":"%%{json.escape(resp.http.Content-Type)}V", "user_agent":"%%{json.escape(req.http.User-Agent)}V", "fastly_backend":"%%{json.escape(resp.http.Fastly-Backend-Name)}V", "data_centre":"%%{json.escape(server.datacenter)}V", "cache_hit":%%{if(fastly_info.state ~"^(HIT|MISS)(?:-|$)", "true", "false")}V, "cache_response":"%%{regsub(fastly_info.state, "^(HIT-(SYNTH)|(HITPASS|HIT|MISS|PASS|ERROR|PIPE)).*", "\\2\\3") }V", "tls_client_protocol":"%%{json.escape(tls.client.protocol)}V", "tls_client_cipher":"%%{json.escape(tls.client.cipher)}V", "client_ja3":"%%{json.escape(req.http.Client-JA3)}V" }
         EOT

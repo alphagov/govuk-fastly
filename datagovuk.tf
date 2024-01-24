8c605e6dda40e64dd129bcd379d88b3f5e3e3433
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2024-01-24 11:14:42 +0000

    Temporarily removes the datagovuk in integration
    This module is failing in fastly. It needs be removed so that other
    changes can still go ahead whilst it is being fixed.

--- datagovuk.tf
+++ datagovuk.tf
@@ -2,14 +2,14 @@ variable "datagovuk_integration" {
   type = string
 }
 
-module "datagovuk-integration" {
-  source = "./modules/datagovuk"
+# module "datagovuk-integration" {
+#   source = "./modules/datagovuk"
 
-  configuration = {
-    environment = "integration"
-    git_hash = var.TFC_CONFIGURATION_VERSION_GIT_COMMIT_SHA
-    probe = "/"
-  }
+#   configuration = {
+#     environment = "integration"
+#     git_hash = var.TFC_CONFIGURATION_VERSION_GIT_COMMIT_SHA
+#     probe = "/"
+#   }
 
-  secrets = yamldecode(var.datagovuk_integration)
-}
+#   secrets = yamldecode(var.datagovuk_integration)
+# }

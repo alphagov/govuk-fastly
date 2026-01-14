b15665174bfd58f605763aa163e9805be2a692d9
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2026-01-14 15:09:15 +0000

    Bump Terraform `required_version` to 1.12
    Description:
    - https://github.com/alphagov/govuk-fastly-secrets is on `v1.12.1` so bump all Fastly workspaces to that
    - https://github.com/alphagov/govuk-infrastructure/issues/2187

--- assets/main.tf
+++ assets/main.tf
@@ -5,7 +5,7 @@ terraform {
       tags = ["fastly", "assets"]
     }
   }
-  required_version = "~> 1.7"
+  required_version = "~> 1.12"
   required_providers {
     fastly = {
       source  = "fastly/fastly"
